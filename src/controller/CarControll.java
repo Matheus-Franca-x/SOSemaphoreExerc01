c708c4f8c8c61aa420b54afe8eaa28e6174b9752
matheus <matheussss6613@gmail.com>
2023-03-14 12:48:49 -0300

    Criação de vetor carro e Thread

--- src/controller/CarControll.java
+++ src/controller/CarControll.java
@@ -1,5 +1,33 @@
 package controller;
 
-public class CarControll {
+public class CarControll 
+{
+	
+	private String cor = "";
+	
+	public CarControll(String cor)
+	{
+		this.cor = cor;
+	}
+	
+	public void inicioCar()
+	{
+		new Thread()
+		{
+			public void run()
+			{
+				
+			}
+		}.start();;
+	}
+	
+	public void moveCar()
+	{
+		
+		
+		
+		
+	}
+	
 
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,22 @@
 package view;
 
-public class Main {
+import controller.CarControll;
+
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		
+		CarControll carro[] = 
+		{
+				new CarControll("Vemelhor"),
+				new CarControll("Azul"),
+				new CarControll("Verde"),
+				new CarControll("Amarelo")
+		};
+		
+		
+		
+	}
 
 }
